259dd0ed6f48b3a7ce26792266b0c70a234cbe7a
Snigur69 <asnigur69@gmail.com>
2020-07-26 14:47:02 +0300

    Updates

---
+++ precache-manifest.2fa7642f3dc201882c5c062c8dbca541.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa681653cdd4902f0d97feda7f6fd433",
+    "url": "/snigur_test_yalantis/index.html"
+  },
+  {
+    "revision": "a31b24948662ba8a7b01",
+    "url": "/snigur_test_yalantis/static/css/main.3bf05000.chunk.css"
+  },
+  {
+    "revision": "c73b4bb287697953daf6",
+    "url": "/snigur_test_yalantis/static/js/2.9e3bc523.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snigur_test_yalantis/static/js/2.9e3bc523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31b24948662ba8a7b01",
+    "url": "/snigur_test_yalantis/static/js/main.6cfde3f8.chunk.js"
+  },
+  {
+    "revision": "4686a8309525883c0127",
+    "url": "/snigur_test_yalantis/static/js/runtime-main.fa1a4f91.js"
+  }
+]);
